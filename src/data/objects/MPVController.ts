56d37ba54ba1a8c5f06ebba63bea8db0f9cf100e
jlc00052 <jlc00052@red.ujaen.es>
2024-12-28 16:42:10 +0100

    MPV working on Windows

--- src/data/objects/MPVController.ts
+++ src/data/objects/MPVController.ts
@@ -7,293 +7,275 @@ import * as fs from "fs";
 import os from "os";
 
 export class MPVController {
-  private mpvProcess: ChildProcessWithoutNullStreams | null = null;
-  //private videoLoaded: boolean = false;
-  private paused: boolean = false;
-  private static ipcSocketPath: string = "//./pipe/mpv-pipe";
-  private clientSocket: net.Socket | null = null;
-  //private connectionEstablished: boolean = false;
+	private mpvProcess: ChildProcessWithoutNullStreams | null = null;
+	private paused: boolean = false;
+	private static ipcSocketPath: string = "";
+	private clientSocket: net.Socket | null = null;
 
-  constructor(private window: BrowserWindow) {}
+	constructor(private window: BrowserWindow) {}
 
-  // Método para iniciar MPV en la ventana de Electron
-  public startMPV(videoPath: string): void {
-    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+	// Método para iniciar MPV en la ventana de Electron
+	public startMPV(videoPath: string): void {
+		const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-    // Ruta del binario de MPV según el sistema operativo
-    const mpvPath =
-      os.platform() === "win32"
-        ? path.join(__dirname, "../src/mpv/mpv.exe")
-        : "mpv";
+		// Ruta del binario de MPV según el sistema operativo
+		const mpvPath =
+			os.platform() === "win32"
+				? path.join(__dirname, "../src/mpv/mpv.exe")
+				: "mpv";
 
-    MPVController.ipcSocketPath = os.platform() === "win32"
-    ? "--input-ipc-server=\\\\.\\pipe\\mpv-pipe" // Windows
-    : "--input-ipc-server=/tmp/mpv-socket";
+		// Crear socket en la ruta de la aplicación
+		MPVController.ipcSocketPath =
+			os.platform() === "win32"
+				? `\\\\.\\pipe\\mpv-${Math.random().toString(36).substring(7)}` // Windows
+				: path.join(
+						__dirname,
+						`mpv-socket-${Math.random().toString(36).substring(7)}`
+				  ); // macOS/Linux
 
-    // Obtiene el identificador nativo de la ventana
-    const nativeHandle = this.window.getNativeWindowHandle();
+		// Obtiene el identificador nativo de la ventana
+		const nativeHandle = this.window.getNativeWindowHandle();
+		let windowId;
+		if (os.platform() === "win32") {
+			windowId = nativeHandle.readInt32LE(0).toString(); // Windows: HWND
+		} else {
+			windowId = "0x" + nativeHandle.toString("hex"); // macOS/Linux: NSView/X11
+		}
 
-    let windowId;
-    if (os.platform() === "win32") {
-      // Windows: HWND
-      windowId = nativeHandle.readInt32LE(0).toString();
-    } else if (os.platform() === "darwin") {
-      // macOS: NSView
-      windowId = "0x" + nativeHandle.toString("hex");
-    } else {
-      // Linux (similar a macOS)
-      windowId = "0x" + nativeHandle.toString("hex");
-    }
+		console.log("Window ID:", windowId);
 
-    console.log("Window ID:", windowId);
+		// Comandos iniciales de MPV
+		const args = [
+			"--wid=" + windowId,
+			"--no-border",
+			"--autofit=100%",
+			`--input-ipc-server=${MPVController.ipcSocketPath}`,
+			"--log-file=mpv.log",
+			`https://seerial.sirjohn.es/video?path=${videoPath}`, // Ruta de tu video
+		];
 
-    // Comandos iniciales de MPV
-    const args = [
-      "--wid=" + windowId,
-      "--no-border",
-      "--autofit=100%",
-      os.platform() === "win32"
-        ? "--input-ipc-server=\\\\.\\pipe\\mpv-pipe" // Windows
-        : "--input-ipc-server=/tmp/mpv-socket", // macOS/Linux
-      "--log-file=mpv.log",
-      `https://seerial.sirjohn.es/video/${videoPath}`,
-    ];
+		// Spawn del proceso MPV
+		this.mpvProcess = spawn(mpvPath, args);
 
-    // Spawn del proceso MPV
-    this.mpvProcess = spawn(mpvPath, args);
+		// Log de MPV para depuración
+		this.mpvProcess.stdout.on("data", (data) => {
+			console.log(`MPV STDOUT: ${data}`);
+		});
 
-    /*this.mpvProcess.stdout.on('data', (data) => {
-      console.log(`MPV STDOUT: ${data}`);
-    });*/
+		this.mpvProcess.stderr.on("data", (data) => {
+			console.error(`MPV STDERR: ${data}`);
+		});
 
-    /*this.mpvProcess.stderr.on('data', (data) => {
-      console.error(`MPV STDERR: ${data}`);
-    });*/
+		this.mpvProcess.on("close", (code) => {
+			console.log(`MPV process exited with code ${code}`);
+		});
 
-    this.mpvProcess.on("close", (code) => {
-      console.log(`MPV process exited with code ${code}`);
-      //this.videoLoaded = true;
-    });
+		// Esperar hasta que el socket esté disponible antes de conectar
+		this.waitForSocketAndConnect();
+	}
 
-    //this.videoLoaded = true;
+	private waitForSocketAndConnect(): void {
+		const checkSocketExists = () => {
+			fs.access(MPVController.ipcSocketPath, fs.constants.F_OK, (err) => {
+				if (err) {
+					console.log("Socket not available, retrying in 100ms...");
+					setTimeout(checkSocketExists, 100);
+				} else {
+					console.log("Socket is available, connecting...");
+					this.connectToMPVSocket();
+				}
+			});
+		};
+		checkSocketExists();
+	}
 
-    // Esperar hasta que el socket esté disponible antes de conectar
-    this.waitForSocketAndConnect();
-  }
+	private connectToMPVSocket(): void {
+		this.clientSocket = net.createConnection(
+			MPVController.ipcSocketPath,
+			() => {
+				console.log("Connected to MPV pipe");
+			}
+		);
 
-  private waitForSocketAndConnect(): void {
-    const checkSocketExists = () => {
-      fs.access(MPVController.ipcSocketPath, fs.constants.F_OK, (err) => {
-        if (err) {
-          console.log("Socket not available, retrying in 100ms...");
-          setTimeout(checkSocketExists, 100); // Reintentar después de 500ms
-        } else {
-          console.log("Socket is available, connecting...");
-          this.connectToMPVSocket();
-        }
-      });
-    };
+		this.clientSocket.on("data", (data) => {
+			console.log("Response from MPV:", data.toString());
+		});
 
-    checkSocketExists();
-  }
+		this.clientSocket.on("error", (err) => {
+			console.error("Client socket error:", err);
+			this.mpvProcess?.kill();
+		});
 
-  private connectToMPVSocket(): void {
-    this.clientSocket = net.createConnection(
-      MPVController.ipcSocketPath,
-      () => {
-        console.log("Connected to MPV pipe");
-      }
-    );
+		this.clientSocket.on("end", () => {
+			console.log("Disconnected from MPV pipe");
+			this.clientSocket = null;
+			this.mpvProcess?.kill();
+		});
+	}
 
-    this.clientSocket.on("data", (data) => {
-      console.log("Response from MPV:", data.toString());
-    });
+	// Método para enviar comandos al reproductor MPV usando named pipes
+	public sendCommand(args: string[] = []): void {
+		if (this.clientSocket) {
+			const command = JSON.stringify({
+				command: args,
+			});
+			this.clientSocket.write(command + "\n", "utf-8", (err) => {
+				if (err) {
+					console.error("Error sending command to MPV:", err);
+				} else {
+					console.log(`Comando enviado: ${args.join(" ")}`);
+				}
+			});
+		} else {
+			console.error("Client socket is not connected.");
+		}
+	}
 
-    this.clientSocket.on("error", (err) => {
-      console.error("Client socket error:", err);
-      this.mpvProcess?.kill();
-    });
+	// Pausar o reanudar el video
+	public togglePause(): void {
+		this.paused = !this.paused;
+		this.sendCommand(["set", "pause", this.paused ? "yes" : "no"]);
+	}
 
-    this.clientSocket.on("end", () => {
-      console.log("Disconnected from MPV pipe");
-      this.clientSocket = null;
-      this.mpvProcess?.kill();
-    });
-  }
+	// Detener el video
+	public stop(): void {
+		this.sendCommand(["stop"]);
+	}
 
-  // Método para enviar comandos al reproductor MPV usando named pipes
-  public sendCommand(args: string[] = []): void {
-    if (this.mpvProcess) {
-      try {
-        // Abrimos el pipe en modo escritura
-        const fd = fs.openSync("\\\\.\\pipe\\mpv-pipe", "w");
+	// Ajustar el volumen
+	public adjustVolume(volume: number): void {
+		this.sendCommand(["set", "volume", volume.toString()]);
+	}
 
-        // Escribimos el comando en el pipe seguido de un salto de línea
-        fs.writeSync(fd, `${args.join(" ")}\n`);
+	// Buscar hacia adelante
+	public seekForward(seconds: number = 10): void {
+		this.sendCommand(["seek", seconds.toString()]);
+	}
 
-        // Cerramos el descriptor de archivo
-        fs.closeSync(fd);
+	// Buscar hacia atrás
+	public seekBackward(seconds: number = -5): void {
+		this.sendCommand(["seek", seconds.toString()]);
+	}
 
-        console.log(`Comando enviado: ${args.join(" ")}`);
-      } catch (error) {
-        console.error("Error al enviar el comando al pipe:", error);
-      }
-    }
-  }
+	// Selección de pistas de video, audio y subtítulos
+	public setVideoTrack(videoId: number): void {
+		this.sendCommand(["set", "vid", videoId.toString()]);
+	}
 
-  // Pausar o reanudar el video
-  public togglePause(): void {
-    this.paused = !this.paused;
+	public setAudioTrack(audioId: number): void {
+		this.sendCommand(["set", "aid", audioId.toString()]);
+	}
 
-    if (this.paused) {
-      this.sendCommand(["set", "pause", "yes"]);
-    } else {
-      this.sendCommand(["set", "pause", "no"]);
-    }
+	public setSubtitleTrack(subtitleId: number): void {
+		this.sendCommand(["set", "sid", subtitleId.toString()]);
+	}
 
-    //this.sendCommand(['cycle', 'pause']);
-  }
+	public disableSubtitles(): void {
+		this.sendCommand(["set", "sid", "no"]);
+	}
 
-  // Detener el video
-  public stop(): void {
-    //this.videoLoaded = false;
-    this.sendCommand(["stop"]);
-  }
+	// Ajuste del gamma
+	public setGamma(gammaValue: number): void {
+		gammaValue = Math.max(-100, Math.min(100, gammaValue)); // Limitar el valor de gamma
+		this.sendCommand(["set", "gamma", gammaValue.toString()]);
+	}
 
-  // Ajustar el volumen
-  public adjustVolume(volume: number): void {
-    this.sendCommand(["set", "volume", volume.toString()]);
-  }
+	// Ajuste del retraso de audio
+	public setAudioDelay(delayMS: number): void {
+		this.sendCommand(["set", "audio-delay", (delayMS / 1000).toString()]);
+	}
 
-  // Buscar hacia adelante
-  public seekForward(seconds: number = 10): void {
-    this.sendCommand(["seek", seconds.toString()]);
-  }
+	// Ajuste del retraso de subtítulos
+	public setSubtitleDelay(delayMS: number): void {
+		this.sendCommand(["set", "sub-delay", (delayMS / 1000).toString()]);
+	}
 
-  // Buscar hacia atrás
-  public seekBackward(seconds: number = -5): void {
-    this.sendCommand(["seek", seconds.toString()]);
-  }
+	// Ajuste del zoom del video
+	public fixZoom(zoomValue: number): void {
+		this.sendCommand(["set", "video-zoom", zoomValue.toString()]);
+	}
 
-  // Selección de pistas de video, audio y subtítulos
-  public setVideoTrack(videoId: number): void {
-    this.sendCommand(["set", "vid", videoId.toString()]);
-  }
+	// Ajuste de la posición vertical de los subtítulos
+	public setSubtitleVerticalPosition(position: number): void {
+		this.sendCommand(["set", "sub-pos", position.toString()]);
+	}
 
-  public setAudioTrack(audioId: number): void {
-    this.sendCommand(["set", "aid", audioId.toString()]);
-  }
+	// Ajuste del tamaño de los subtítulos
+	public setSubtitleSize(size: number): void {
+		this.sendCommand(["set", "sub-scale", size.toString()]);
+	}
 
-  public setSubtitleTrack(subtitleId: number): void {
-    this.sendCommand(["set", "sid", subtitleId.toString()]);
-  }
+	// Configurar la interpolación
+	public configureInterpolation(enable: boolean): void {
+		if (enable) {
+			this.sendCommand(["set", "video-sync", "display-resample"]);
+			this.sendCommand(["set", "interpolation", "yes"]);
+			this.sendCommand(["set", "tscale", "sphinx"]);
+			this.sendCommand(["set", "tscale-blur", "0.6991556596428412"]);
+			this.sendCommand(["set", "tscale-radius", "1.05"]);
+			this.sendCommand(["set", "tscale-clamp", "0.0"]);
+		} else {
+			this.sendCommand(["set", "interpolation", "no"]);
+		}
+	}
 
-  public disableSubtitles(): void {
-    this.sendCommand(["set", "sid", "no"]);
-  }
+	/**
+	 * Configure video quality settings
+	 */
+	public configureQualitySettings(highQuality: boolean): void {
+		if (highQuality) {
+			this.sendCommand(["set", "gpu-api", "vulkan"]);
+			this.sendCommand(["set", "gpu-context", "auto"]);
+			this.sendCommand(["set", "profile", "high-quality"]);
+			this.sendCommand(["set", "vo", "gpu-next"]);
+			this.sendCommand(["set", "hwdec", "auto-safe"]);
+		} else {
+			this.sendCommand(["set", "gpu-api", "d3d11"]);
+			this.sendCommand(["set", "gpu-context", "d3d11"]);
+			this.sendCommand(["set", "profile", "fast"]);
+			this.sendCommand(["set", "vo", "gpu-next"]);
+			this.sendCommand(["set", "hwdec", "d3d11va"]);
+		}
+	}
 
-  // Ajuste del gamma
-  public setGamma(gammaValue: number): void {
-    gammaValue = Math.max(-100, Math.min(100, gammaValue)); // Limitar el valor de gamma
-    this.sendCommand(["set", "gamma", gammaValue.toString()]);
-  }
+	/**
+	 * Configure video player settings
+	 */
+	public configureSettings(): void {
+		//HDR Settings
+		this.sendCommand(["set", "tone-mapping", "bt.2446a"]);
+		this.sendCommand(["set", "hdr-peak-percentile", "99.995"]);
+		this.sendCommand(["set", "hdr-contrast-recovery", "0.30"]);
+		this.sendCommand(["set", "target-colorspace-hint", "yes"]);
+		this.sendCommand(["set", "target-contrast", "auto"]);
 
-  // Ajuste del retraso de audio
-  public setAudioDelay(delayMS: number): void {
-    this.sendCommand(["set", "audio-delay", (delayMS / 1000).toString()]);
-  }
+		//Audio Settings
+		this.sendCommand(["set", "audio-stream", "silence"]);
+		this.sendCommand(["set", "audio-pitch-correction", "yes"]);
 
-  // Ajuste del retraso de subtítulos
-  public setSubtitleDelay(delayMS: number): void {
-    this.sendCommand(["set", "sub-delay", (delayMS / 1000).toString()]);
-  }
+		//Subtitle Settings
+		this.sendCommand(["set", "blend-subtitles", "no"]);
+		this.sendCommand(["set", "demuxer-mkv-subtitle-preroll", "yes"]);
+		this.sendCommand(["set", "embeddedfonts", "yes"]);
+		this.sendCommand(["set", "sub-fix-timing", "no"]);
+		this.sendCommand(["set", "sub-font", "Open Sans SemiBold"]);
+		this.sendCommand(["set", "sub-font-size", "46"]);
+		this.sendCommand(["set", "sub-blur", "0.3"]);
+		this.sendCommand(["set", "sub-border-color", "0.0/0.0/0.0/0.8"]);
+		this.sendCommand(["set", "sub-border-size", "3.2"]);
+		this.sendCommand(["set", "sub-color", "0.9/0.9/0.9/1.0"]);
+		this.sendCommand(["set", "sub-margin-x", "100"]);
+		this.sendCommand(["set", "sub-margin-y", "50"]);
+		this.sendCommand(["set", "sub-shadow-color", "0.0/0.0/0.0/0.25"]);
+		this.sendCommand(["set", "sub-shadow-offset", "0"]);
 
-  // Ajuste del zoom del video
-  public fixZoom(zoomValue: number): void {
-    this.sendCommand(["set", "video-zoom", zoomValue.toString()]);
-  }
-
-  // Ajuste de la posición vertical de los subtítulos
-  public setSubtitleVerticalPosition(position: number): void {
-    this.sendCommand(["set", "sub-pos", position.toString()]);
-  }
-
-  // Ajuste del tamaño de los subtítulos
-  public setSubtitleSize(size: number): void {
-    this.sendCommand(["set", "sub-scale", size.toString()]);
-  }
-
-  // Configurar la interpolación
-  public configureInterpolation(enable: boolean): void {
-    if (enable) {
-      this.sendCommand(["set", "video-sync", "display-resample"]);
-      this.sendCommand(["set", "interpolation", "yes"]);
-      this.sendCommand(["set", "tscale", "sphinx"]);
-      this.sendCommand(["set", "tscale-blur", "0.6991556596428412"]);
-      this.sendCommand(["set", "tscale-radius", "1.05"]);
-      this.sendCommand(["set", "tscale-clamp", "0.0"]);
-    } else {
-      this.sendCommand(["set", "interpolation", "no"]);
-    }
-  }
-
-  /**
-   * Configure video quality settings
-   */
-  public configureQualitySettings(highQuality: boolean): void {
-    if (highQuality) {
-      this.sendCommand(["set", "gpu-api", "vulkan"]);
-      this.sendCommand(["set", "gpu-context", "auto"]);
-      this.sendCommand(["set", "profile", "high-quality"]);
-      this.sendCommand(["set", "vo", "gpu-next"]);
-      this.sendCommand(["set", "hwdec", "auto-safe"]);
-    } else {
-      this.sendCommand(["set", "gpu-api", "d3d11"]);
-      this.sendCommand(["set", "gpu-context", "d3d11"]);
-      this.sendCommand(["set", "profile", "fast"]);
-      this.sendCommand(["set", "vo", "gpu-next"]);
-      this.sendCommand(["set", "hwdec", "d3d11va"]);
-    }
-  }
-
-  /**
-   * Configure video player settings
-   */
-  public configureSettings(): void {
-    //HDR Settings
-    this.sendCommand(["set", "tone-mapping", "bt.2446a"]);
-    this.sendCommand(["set", "hdr-peak-percentile", "99.995"]);
-    this.sendCommand(["set", "hdr-contrast-recovery", "0.30"]);
-    this.sendCommand(["set", "target-colorspace-hint", "yes"]);
-    this.sendCommand(["set", "target-contrast", "auto"]);
-
-    //Audio Settings
-    this.sendCommand(["set", "audio-stream", "silence"]);
-    this.sendCommand(["set", "audio-pitch-correction", "yes"]);
-
-    //Subtitle Settings
-    this.sendCommand(["set", "blend-subtitles", "no"]);
-    this.sendCommand(["set", "demuxer-mkv-subtitle-preroll", "yes"]);
-    this.sendCommand(["set", "embeddedfonts", "yes"]);
-    this.sendCommand(["set", "sub-fix-timing", "no"]);
-    this.sendCommand(["set", "sub-font", "Open Sans SemiBold"]);
-    this.sendCommand(["set", "sub-font-size", "46"]);
-    this.sendCommand(["set", "sub-blur", "0.3"]);
-    this.sendCommand(["set", "sub-border-color", "0.0/0.0/0.0/0.8"]);
-    this.sendCommand(["set", "sub-border-size", "3.2"]);
-    this.sendCommand(["set", "sub-color", "0.9/0.9/0.9/1.0"]);
-    this.sendCommand(["set", "sub-margin-x", "100"]);
-    this.sendCommand(["set", "sub-margin-y", "50"]);
-    this.sendCommand(["set", "sub-shadow-color", "0.0/0.0/0.0/0.25"]);
-    this.sendCommand(["set", "sub-shadow-offset", "0"]);
-
-    //Other Settings
-    this.sendCommand(["set", "deinterlace", "no"]);
-    this.sendCommand(["set", "dither-depth", "auto"]);
-    this.sendCommand(["set", "deband", "yes"]);
-    this.sendCommand(["set", "deband-iterations", "4"]);
-    this.sendCommand(["set", "deband-threshold", "35"]);
-    this.sendCommand(["set", "deband-range", "16"]);
-    this.sendCommand(["set", "deband-grain", "4"]);
-  }
+		//Other Settings
+		this.sendCommand(["set", "deinterlace", "no"]);
+		this.sendCommand(["set", "dither-depth", "auto"]);
+		this.sendCommand(["set", "deband", "yes"]);
+		this.sendCommand(["set", "deband-iterations", "4"]);
+		this.sendCommand(["set", "deband-threshold", "35"]);
+		this.sendCommand(["set", "deband-range", "16"]);
+		this.sendCommand(["set", "deband-grain", "4"]);
+	}
 }
